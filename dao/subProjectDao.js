ab51e91e914c95945afa74b0ef5230b6217e3508
Sarikhin <sarikhin.tubari@agungsedayu.com>
2023-04-07 23:46:50 +0700

    [update] add project in sub project response

--- dao/subProjectDao.js
+++ dao/subProjectDao.js
@@ -2,11 +2,35 @@ const SuperDao = require("./superDao");
 const models = require("../models");
 const SubProject = models.sub_project;
 const logger = require("../config/logger");
+const Project = models.project;
+
 
 
 class SubProjectDao extends SuperDao {
   constructor() {
     super(SubProject);
+  }
+  async getDataTableData(
+    where,
+    limit,
+    offset,
+    order = [["created_at", "DESC"]]
+  ) {
+    return SubProject.findAndCountAll({
+      limit: parseInt(limit, 10),
+      offset: parseInt(offset, 10),
+      include: Project,
+      where,
+      order,
+    })
+      .then((result) => {
+        return result;
+      })
+      .catch((e) => {
+        logger.error(e);
+        console.log(e);
+        return [];
+      });
   }
   async findByCode(code) {
     return SubProject.findOne({ where: { code } });
